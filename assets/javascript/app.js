499f9bf2413bbd521e3eb934121391b48fb29cbc
arhee <arheeee@gmail.com>
2018-03-03 13:18:28 -0800

    all search functions created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -73,7 +73,7 @@ $("#add2").on("click", function () {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        var beer = Math.round(Math.random() * response.length);
+        var beer = Math.floor(Math.random() * response.length);
         beerChoice = response[beer];
         console.log(beerChoice);
         beerName = beerChoice.name;
@@ -90,11 +90,127 @@ $("#add2").on("click", function () {
         console.log(beerFood1);
         console.log(beerFood2);
         console.log(beerFood3);
-        beerMovie = beerTag.substring(0, 5);
-        console.log(beerMovie);
+        brewYear = beerChoice.first_brewed;
+        console.log(brewYear);
+        year = brewYear.substring(3, 7);
+        console.log(year);
+
+        var queryMovieDB = "https://api.themoviedb.org/3/discover/movie?api_key=65e7259520279f0add439f24bca07ecb&sort_by=popularity.desc&primary_release_year=" + year
+
+        $.ajax({
+            url: queryMovieDB,
+            method: "GET"
+        }).then(function (movData) {
+            console.log(movData);
+            var randomNum = Math.floor(Math.random() * 15);
+            var movName = movData.results[randomNum].title;
+            console.log(movName);
+
+            var queryMovie = "https://omdbapi.com/?apikey=trilogy&t=" + movName;
+
+            $.ajax({
+                url: queryMovie,
+                method: "GET"
+            }).then(function (data) {
+                console.log(data);
+                var rating = data.Ratings[0].Value;
+                var rawRating = rating.substring(0, 3);
+                var ratingNumber = Math.round(rawRating);
+                var title = data.Title;
+                console.log(title);
+                var actors = data.Actors;
+                console.log(actors);
+                var poster = data.Poster;
+                console.log(poster);
+                
+            })
+
+
+        })
+
+
+
+
+
     })
 
-})
+});
+
+$("#add3").on("click", function () {
+
+    event.preventDefault();
+
+    var food = $("#input3").val().trim();
+
+    var queryFood = "https://api.punkapi.com/v2/beers?food=" + food;
+
+    $.ajax({
+        url: queryFood,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var beer = Math.floor(Math.random() * response.length);
+        beerChoice = response[beer];
+        console.log(beerChoice);
+        beerName = beerChoice.name;
+        console.log(beerName);
+        beerTag = beerChoice.tagline;
+        console.log(beerTag);
+        beerDescription = beerChoice.description;
+        console.log(beerDescription);
+        beerImage = beerChoice.image_url;
+        console.log(beerImage);
+        beerFood1 = beerChoice.food_pairing[0];
+        beerFood2 = beerChoice.food_pairing[1];
+        beerFood3 = beerChoice.food_pairing[2];
+        console.log(beerFood1);
+        console.log(beerFood2);
+        console.log(beerFood3);
+        brewYear = beerChoice.first_brewed;
+        console.log(brewYear);
+        year = brewYear.substring(3, 7);
+        console.log(year);
+
+        var queryMovieDB = "https://api.themoviedb.org/3/discover/movie?api_key=65e7259520279f0add439f24bca07ecb&sort_by=popularity.desc&primary_release_year=" + year
+
+        $.ajax({
+            url: queryMovieDB,
+            method: "GET"
+        }).then(function (movData) {
+            console.log(movData);
+            var randomNum = Math.floor(Math.random() * 15);
+            var movName = movData.results[randomNum].title;
+            console.log(movName);
+
+            var queryMovie = "https://omdbapi.com/?apikey=trilogy&t=" + movName;
+
+            $.ajax({
+                url: queryMovie,
+                method: "GET"
+            }).then(function (data) {
+                console.log(data);
+                var rating = data.Ratings[0].Value;
+                var rawRating = rating.substring(0, 3);
+                var ratingNumber = Math.round(rawRating);
+                var title = data.Title;
+                console.log(title);
+                var actors = data.Actors;
+                console.log(actors);
+                var poster = data.Poster;
+                console.log(poster);
+                
+            })
+
+
+        })
+
+
+
+
+
+    })
+
+});
 
 
 
